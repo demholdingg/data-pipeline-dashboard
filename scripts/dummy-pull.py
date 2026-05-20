d93bba9ebc8bfe1ff54c3cbcbd20869e7631d995
demholdingg <it@dutaenergi.co.id>
2026-05-20 13:39:37 +0700

    first commit

---
+++ scripts/dummy-pull.py
@@ -0,0 +1,8 @@
+import time
+
+print("Memulai proses penarikan data...")
+time.sleep(2)
+print("Berhasil mensimulasikan koneksi API!")
+print("Menyimpan data ke PostgreSQL di Synology...")
+time.sleep(2)
+print("Pipeline PoC Berjalan Sukses dari Runner Synology!")
